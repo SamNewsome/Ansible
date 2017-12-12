3fc26f63c922293c2409426e506d48a1ec92436e
SamNewsome <samuel.newsome@qa.com>
2017-12-12 11:13:09 +0000

    Second commit

--- parent_bootstrap.sh
+++ parent_bootstrap.sh
@@ -3,10 +3,10 @@
 #Install packages
 sudo apt-get update
 sudo apt-get -y openssh-server openssh-client
-sudo ufw disable
 sudo apt-get -y install ansible
 
 #Edit /etc/ansible/hosts
 fqdn=$(facter fqdn)
 ip=$(facter ipaddress_eth1)
-sudo sed -i "$ip" /etc/ansible/hosts
+sudo sed '$ a\ > [hosts]' /etc/ansible/hosts
+sudo sed '$ a\ > 192.168.1.170' /etc/ansible/hosts
